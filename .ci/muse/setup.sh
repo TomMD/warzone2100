f075d42057f9a67a0c94f83ac84bd26914a81b65
past-due <30942300+past-due@users.noreply.github.com>
2020-11-28 17:38:20 -0500

    Tweaks 3

--- .ci/muse/setup.sh
+++ .ci/muse/setup.sh
@@ -2,11 +2,16 @@
 
 # Reference: https://docs.muse.dev/docs/configuring-muse/
 
-apt update
-apt install -y wget ninja-build git gettext asciidoctor zip unzip
-apt install -y libpng-dev libsdl2-dev libopenal-dev libphysfs-dev libvorbis-dev libtheora-dev libxrandr-dev qtscript5-dev qt5-default libfribidi-dev libfreetype6-dev libharfbuzz-dev libfontconfig1-dev libcurl4-gnutls-dev gnutls-dev libsodium-dev
+apt-get -u update
+DEBIAN_FRONTEND=noninteractive apt-get -y install wget ninja-build git gettext asciidoctor zip unzip
+DEBIAN_FRONTEND=noninteractive apt-get -y install libpng-dev libsdl2-dev libopenal-dev libphysfs-dev libvorbis-dev libtheora-dev libxrandr-dev qtscript5-dev qt5-default libfribidi-dev libfreetype6-dev libharfbuzz-dev libfontconfig1-dev libcurl4-gnutls-dev gnutls-dev libsodium-dev
 
-wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc | apt-key add -
+echo "Installing Vulkan SDK..."
+echo "wget lunarg signing-key"
+wget -qO - https://packages.lunarg.com/lunarg-signing-key-pub.asc| apt-key add -
+echo "wget sources.list.d"
 wget -qO /etc/apt/sources.list.d/lunarg-vulkan-1.2.148-focal.list https://packages.lunarg.com/vulkan/1.2.148/lunarg-vulkan-1.2.148-focal.list
-apt update
-apt install vulkan-sdk
+echo "apt-get -u update"
+apt-get -u update
+echo "apt-get -y install vulkan-sdk"
+DEBIAN_FRONTEND=noninteractive apt-get -y install vulkan-sdk
