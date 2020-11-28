6250a277522992fc25491196c0ae55942ccc565c
past-due <30942300+past-due@users.noreply.github.com>
2020-11-28 17:57:43 -0500

    Tweak 6

--- .ci/muse/setup.sh
+++ .ci/muse/setup.sh
@@ -5,3 +5,6 @@
 apt-get -u update
 DEBIAN_FRONTEND=noninteractive apt-get -y install wget ninja-build git gettext asciidoctor zip unzip
 DEBIAN_FRONTEND=noninteractive apt-get -y install libpng-dev libsdl2-dev libopenal-dev libphysfs-dev libvorbis-dev libtheora-dev libxrandr-dev qtscript5-dev qt5-default libfribidi-dev libfreetype6-dev libharfbuzz-dev libfontconfig1-dev libcurl4-gnutls-dev gnutls-dev libsodium-dev
+
+# Initialize Git submodules
+git submodule update --init --recursive
